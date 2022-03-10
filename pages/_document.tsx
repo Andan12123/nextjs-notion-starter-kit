ab32143e60aa1315072e620c7988c926ab403ec4
Andan12123 <101311703+Andan12123@users.noreply.github.com>
2022-03-10 17:29:00 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,7 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
-
+            
             <link
               rel='apple-touch-icon'
               sizes='180x180'
